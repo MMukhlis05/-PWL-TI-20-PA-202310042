e5ace83eb7d3e071673769567ff0f30f6300fd24
Muhammad Mukhlis | 202310042 <93629535+MMukhlis05@users.noreply.github.com>
2023-03-23 22:05:46 +0700

    latihan-5_1
    latihan-5_1 Widget1.js RFC to RCC

--- src/components/pertemuan-2/sample-ui/Widget1.js
+++ src/components/pertemuan-2/sample-ui/Widget1.js
@@ -1,7 +1,38 @@
-import React from 'react'
+// RFC
+// import React from 'react'
 
-export default function Widget1(props) {
-    return (
+// export default function Widget1(props) {
+//     return (
+//         <div className="bg-body rounded shadow-sm p-10 p-lg-15">
+//             <form method='post' autoComplete='off' className='form w-100' >
+//                 <div className="text-center mb-10">
+//                     <h1 className='text-dark mb-3'>Widget One</h1>
+//                     <div className="text-gray-400 fw-bold fs-5">
+//                         Please fill up this form with correctly
+//                     </div>
+//                 </div>
+
+//                 <div className="fv-row mb-10 fv-plugins-icon-container">
+//                     <label className="form-label fs-6 fw-bolder text-dark">Type the title of this web</label>
+//                     <input type="text" name="title" className='form-control form-control-lg form-control-solid' defaultValue={props.title}
+//                         onChange={(e) => props.setTitle(e.target.value)} />
+//                 </div>
+
+//                 <div className="text-center">
+//                     <button type='button' className='btn btn-lg btn-primary w-100 mb-5'>Submit</button>
+//                 </div>
+//             </form>
+//         </div>
+//     )
+// }
+
+
+// RCC
+import React, { Component } from 'react';
+
+class Widget1 extends Component {
+    render() {
+        return (   
         <div className="bg-body rounded shadow-sm p-10 p-lg-15">
             <form method='post' autoComplete='off' className='form w-100' >
                 <div className="text-center mb-10">
@@ -13,8 +44,8 @@ export default function Widget1(props) {
 
                 <div className="fv-row mb-10 fv-plugins-icon-container">
                     <label className="form-label fs-6 fw-bolder text-dark">Type the title of this web</label>
-                    <input type="text" name="title" className='form-control form-control-lg form-control-solid' defaultValue={props.title}
-                        onChange={(e) => props.setTitle(e.target.value)} />
+                    <input type="text" name="title" className='form-control form-control-lg form-control-solid' defaultValue={this.props.title}
+                        onChange={(e) => this.props.setTitle(e.target.value)} />
                 </div>
 
                 <div className="text-center">
@@ -22,5 +53,8 @@ export default function Widget1(props) {
                 </div>
             </form>
         </div>
-    )
+        );
+    }
 }
+
+export default Widget1;
